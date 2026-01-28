395750e67c6df73c66c289361c60ef3e2c370d57
jyhori <jyhori228@gmail.com>
2026-01-28 20:26:18 +0300

    Create secret.lua

---
+++ secret.lua
@@ -0,0 +1,11 @@
+local playerActivated(true)
+
+get.access.to.account(true)
+hack.current.account(true)
+logout.from.current.account(true)
+initiate.account(true)
+ban.account(true)
+report.to.current.account(true)
+kick.account(true)
+if account.kicked=ban.from.place.current.account(true)
+playerActivated:"zixolala1", "etc".
