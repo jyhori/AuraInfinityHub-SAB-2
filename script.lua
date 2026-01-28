881d021d0e7cab858a0d47f6cbed1d56dae269ee
jyhori <jyhori228@gmail.com>
2026-01-28 20:14:29 +0300

    scriptAuraHubInfinity.lua

---
+++ script.lua
@@ -0,0 +1,74 @@
+local Player.Playing()
+local Player.Action()
+local Script.Work()
+local Script.Action()
+local gui()
+local Gui.Host()
+
+-- create gui:
+
+Gui.Host = true(id="1234"):
+    connect.password(password="12345")
+    set.id="12345" after.password=true
+    set XYZ = "Down", "Up", "Left", "Right", Player.Action(seted)
+    connect.password(password="1234") chance=1%(true)
+
+-- Real Gui
+
+local screenGui = Instance.new("ScreenGui")
+
+screenGui.Name = "AuraInfinityHub"
+
+screenGui.ResetOnSpawn = false
+
+screenGui.Parent = player:WaitForChild("PlayerGui")
+
+-- Frame
+
+-- Frame
+
+local frame = Instance.new("Frame")
+
+frame.Size = UDim2.new(0, 200, 0, 150)
+
+frame.Position = UDim2.new(0.5, -100, 0.1, 0)
+
+frame.BackgroundColor3 = Color3.fromRGB(20, 20, 20)
+
+frame.BackgroundTransparency = 0
+
+frame.BorderSizePixel = 0
+
+frame.Active = true
+
+frame.Draggable = true
+
+frame.Parent = screenGui
+
+-- Additional GUI code continues...
+
+-- Seted Hub
+
+local TextButton = Instance.new("TextButton")
+
+local SpawnerBrainrot(true)
+
+local Activated(true)
+
+TextButton.Name = "Spawner Brainrot"
+
+if activate.click = set SpawnerBrainrot.Activated
+  screenGui():
+      TextButton.Name = "Spawn Strawberry Elephant".click=Activated=TextButton():
+          spawn "Strawberry Elephant" * Strawberry Elephant(true).SystemAction.consoleGame:Execute+spawned="Strawberry Elephant"{count 1} * action.spawn.brainrot(place:"Steal A Brainrot")=true,
+          set * TextButton + "Loop" action.activated.check.system = "Yes" / "No", change.color from #Grey = #Green * region.of.button=true(spawn "Strawberry Elephant"if loop=on:spawn="Strawberry Elephant" * loop + loop.activated.XYZ.change=to{loop}:
+              loop.activated to.spawn "Strawberry Elephant":loop
+      Text.Button.Name = "Off Brainrot Spawner":
+          "You Are Sure?"
+          Text.Button.Create = "Yes"
+          Text.Button.Create = "No"
+          if Yes=off.SpawnerBrainrot
+          if No=action.cancel(local,cancel,XYZ):
+              -- Yes, you dont leave.
+          activate."Strawberry Elephant"=true()
+        end
