dc282d36663e32348893c28ab78c5ff27cda6b2d
jyhori <jyhori228@gmail.com>
2026-01-28 20:19:08 +0300

    Create script2.lua

---
+++ script2.lua
@@ -0,0 +1,8 @@
+#spawn "Strawberry Elephant" * if.script=activated(true) + {count 1},
+#localCreate place.Support:"Steal A Brainrot" * to.end()
+connect.place="Steal A Brainrot"
+console="/spawn strawberry elephant{count 1}"
+#spawn strawberry elephant{count 1},
+spawn strawberry elephant{count 1},
+-- if doesn't work...
+palce.connect.spawn"StrawberryElephant"in."Steal A Brainrot".roblox(Local,place.roblox.connect.LocalSystem*xxx)
